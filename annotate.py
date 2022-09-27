d2e1596156af731c7a80af8ceb39331895ff73b4
sa48k <sa48k@users.noreply.github.com>
2022-09-27 23:12:17 +1300

    Initial commit

---
+++ annotate.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+A command line tool for annotating .hsreplay files with GameTag data to
+facilitate development activities.
+"""
+import os
+import sys
+from argparse import ArgumentParser, ArgumentTypeError, FileType
+from datetime import datetime
+
+from hsreplay.utils import annotate_replay
+
+
+def date_arg(s):
+	try:
+		return datetime.strptime(s, "%Y-%m-%d")
+	except ValueError as e:
+		raise ArgumentTypeError(e)
+
+
+def main():
+	parser = ArgumentParser(description=__doc__)
+	parser.add_argument(
+		"infile", nargs="?", type=FileType("r", encoding="utf-8"), default=sys.stdin,
+		help="the input replay data"
+	)
+
+	args = parser.parse_args()
+	with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as outfile:
+		annotate_replay(args.infile, outfile)
+
+
+if __name__ == "__main__":
+	main()
