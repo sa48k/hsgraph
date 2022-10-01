6cae5a1b62e06e3d57a960c9ad50efc013a5d931
sa48k <sa48k@users.noreply.github.com>
2022-10-01 22:37:19 +1300

    Added replay featuring Hero cards being played - this screws everything up at present

--- hshpgraph.py
+++ hshpgraph.py
@@ -1,7 +1,8 @@
 from lxml import etree
 
 # tree = etree.parse("./data/PriestWinT5.xml")
-tree = etree.parse("./data/BeastHunterWin_annotated.xml")
+# tree = etree.parse("./data/BeastHunterWin_annotated.xml")
+tree = etree.parse("./data/BeastHunterWin2_annotated.xml") # https://hsreplay.net/replay/gRfknupKptbsozKXa7DnZk
 
 players = tree.xpath('//Player')
 damages = tree.xpath('//Block//MetaData[@MetaName="DAMAGE"]')
@@ -11,7 +12,7 @@ player2 = {}
 
 player1['id'] = players[0].get('playerID')
 player1['name'] = players[0].get('name').split('#')[0]
-player1['entityid'] = players[0].xpath('//Tag[@GameTagName = "HERO_ENTITY"] ')[0].get('value')
+player1['entityid'] = players[0].xpath('//Tag[@GameTagName = "HERO_ENTITY"] ')[0].get('value') # TODO: This changes e.g. when Hero Cards are played
 player1['hero'] = tree.xpath('//FullEntity//Tag[@value="' + player1['entityid'] + '"]/parent::* ')[0].get('EntityName')
 
 player2['id'] = players[1].get('playerID')
@@ -24,7 +25,10 @@ dmg_target_entities = [d.getchildren()[0].get('entity') for d in damages]
 dmg_target_names = [d.getchildren()[0].get('EntityName') for d in damages]
 dmg_zip = list(zip(dmg_amounts, dmg_target_entities, dmg_target_names))
 
-print('Damages that occurred in this match (dmg amount, target entityid, target name):\n', dmg_zip)
+print('Damages that occurred in this match (dmg amount, target entityid, target name):\n')
+      
+for d in dmg_zip:
+    print(d)
 
 # We can retrieve the Hero's damage (maybe) using:
 # <TagChange entity="64" tag="44" value="23" EntityCardID="HERO_09b" EntityCardName="Madame Lazul" GameTagName="DAMAGE"/>
@@ -36,3 +40,5 @@ healthtree2 = tree.xpath('//TagChange[@entity="' + player2['entityid'] + '"][@ta
 player2['health'] = [d.get('value') for d in healthtree2]
 
 print('Players:\n', player1, '\n', player2)
+
+# TODO: A better way might be to iterate over each turn, then calculate each Hero's health at the end of that turn
