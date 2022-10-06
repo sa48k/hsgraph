2ff31b8aeb0007a8e658c4df2b21110c7eb9c642
sa48k <sa48k@users.noreply.github.com>
2022-10-06 23:47:49 +1300

    Basic Hero HP calculation

--- hshpgraph.py
+++ hshpgraph.py
@@ -1,10 +1,10 @@
 from lxml import etree
 
-tree = etree.parse("./data/PriestWinT5.xml")
-# tree = etree.parse("./data/BeastHunterWin_annotated.xml") # https://hsreplay.net/replay/LQ2unSYf7w7hLfBu5Foa6U
+# tree = etree.parse("./data/PriestWinT5.xml")
+tree = etree.parse("./data/BeastHunterWin_annotated.xml") # https://hsreplay.net/replay/LQ2unSYf7w7hLfBu5Foa6U
 # tree = etree.parse("./data/BeastHunterWin2_annotated.xml") # https://hsreplay.net/replay/gRfknupKptbsozKXa7DnZk
 # tree = etree.parse("./data/Healfest_annotated.xml") # priest healfest
-tree = etree.parse("./data/Armorfest_annotated.xml") # warrior armorfest
+# tree = etree.parse("./data/Armorfest_annotated.xml") # warrior armorfest
 
 players = tree.xpath('//Player')
 damages = tree.xpath('//Block//MetaData[@MetaName="DAMAGE"]')
@@ -28,33 +28,49 @@ player2['entityid'] = players[1].xpath('Tag[@GameTagName = "HERO_ENTITY"] ')[0].
 player2['hero'] = tree.xpath('//FullEntity//Tag[@value="' + player2['entityid'] + '"]/parent::* ')[0].get('EntityName')
 player2['damaged'] = 0
 player2['healed'] = 0
-player1['armor'] = 0
+player2['armor'] = 0
 
 dmg_amounts = [d.get('data') for d in damages]
 dmg_target_entities = [d.getchildren()[0].get('entity') for d in damages]
 dmg_target_names = [d.getchildren()[0].get('EntityName') for d in damages]
 dmg_zip = list(zip(dmg_amounts, dmg_target_entities, dmg_target_names))
 
-# healthtree1 = tree.xpath('//TagChange[@entity="' + player1['entityid'] + '"][@tag="44"]  ') 
-# player1['health'] = [d.get('value') for d in healthtree1]
-# healthtree2 = tree.xpath('//TagChange[@entity="' + player2['entityid'] + '"][@tag="44"]') 
-# player2['health'] = [d.get('value') for d in healthtree2]
-
 print('Players:\n', player1, '\n', player2)
 
-# change of turn looks like this:
-# <TagChange entity="1" tag="20" value="19" EntityCardID="GameEntity" EntityCardName="GameEntity" GameTagName="TURN"/>
-#                             19th turn /   (both players take turns, so this is T10 in-game)
+# from tree, get these events: next turn | damage | healing | armour | hero card
+# Next turn:
+# <TagChange entity="1" tag="20" value="1" EntityCardID="GameEntity" EntityCardName="GameEntity" GameTagName="TURN"/>
+#
+# Damage:
+# <TagChange entity="39" tag="44" value="1" EntityCardID="SW_319" EntityCardName="Peasant" GameTagName="DAMAGE"/>
+#
+# Healing:
+# <MetaData meta="2" data="6" infoCount="1" MetaName="HEALING"> 
+#   <Info index="0" entity="18" EntityName="Kurtrus, Demon-Render"/> 
+# </MetaData>
+#
+# Armour:
+# <TagChange entity="18" tag="292" value="4" EntityCardID="AV_204" EntityCardName="Kurtrus, Demon-Render" GameTagName="ARMOR"/>
+#
+# Hero card:
+# <ShowEntity entity="18" cardID="AV_204" EntityName="Kurtrus, Demon-Render"> 
+#   <Tag tag="202" value="3" GameTagName="CARDTYPE"/>
+#   <Tag tag="53" value="18" GameTagName="ENTITY_ID"/>
+#   <Tag tag="292" value="5" GameTagName="ARMOR"/> 
+# </ShowEntity>
 
-# from tree, get these events: next turn | damage | healing | armour
-turns = tree.xpath('//Block//TagChange[@entity="1"][@tag="20"] | //Block//TagChange[@tag="44"] | //Block//TagChange[@tag="425"] | //Block//TagChange[@tag="292"]')
+turns = tree.xpath('//Block//TagChange[@entity="1"][@tag="20"] | //Block//TagChange[@tag="44"] | //Block//MetaData[@meta="2"] | //Block//TagChange[@tag="292"] | //Block[@type="7"]//Tag[@tag="202"][@value="3"]/ancestor::ShowEntity')
 currentturn = 1
 
 for turn in turns:
+    # print(turn.items()) # debug
     if turn.get('tag') == '20':                 # next turn
         currentturn += 1
-        # print(f"{player1['hero']}: received {player1['damaged']} damage")
-        # print(f"{player2['hero']}: received {player2['damaged']} damage")
+        print('End of turn HP:')
+        p1hp = 30 - int(player1["damaged"]) + int(player1["healed"])
+        p2hp = 30 - int(player2["damaged"]) + int(player2["healed"])
+        print(f'{player1["hero"]}: {p1hp} ')
+        print(f'{player2["hero"]}: {p2hp} ')
         print(f'\nTurn {int(currentturn/2)}')
     
     targetid = turn.get('entity')
@@ -70,15 +86,13 @@ for turn in turns:
         elif targetid == player2['entityid']:
             player2['damaged'] = int(dmg)
         print(f'{targetname} (ID: {targetid}) has received {dmg} damage')
-        # print(turn.items())
         
-    if turn.get('tag') == '425':                 # heals target
-        targetname = turn.get('EntityCardName')
-        heal = turn.get('value')
-        if heal == '0':
-            continue
+    if turn.get('meta') == '2':                 # heals target
+        targetid = turn.getchildren()[0].get('entity')
+        targetname = turn.getchildren()[0].get('EntityName')
+        heal = turn.get('data')
         if targetid == player1['entityid']:     # keep track of healing so far
-            player1['healed'] += int(heal)      # BUG: allows for 'overhealing'. Use <MetaData> to fix?
+            player1['healed'] += int(heal)
         elif targetid == player2['entityid']:
             player2['healed'] += int(heal)
         else:
