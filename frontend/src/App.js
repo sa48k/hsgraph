1b3045dfa802c2cf93729b581d11111db9a722dc
sa48k <sa48k@users.noreply.github.com>
2022-10-16 20:48:43 +1300

    MUI Dashboard template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,15 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { Drawer, ListItem, ListItemIcon, ListItemText, Button } from "@material-ui/core";
-import Sidebar from './components/Sidebar'
-import Header from './components/Header'
+// import { Drawer, ListItem, ListItemIcon, ListItemText, Button } from "@material-ui/core";
+// import Sidebar from './components/Sidebar'
+// import Header from './components/Header'
+import Dashboard from './components/Dashboard'
 
 export default function App() {
   return (
     <Box>
-      <Sidebar />
-      <Container style={{ backgroundColor: "red" }}>
-        <h3>Hello World !!</h3>
-      </Container>
+      <Dashboard />
     </Box>
   );
 }
